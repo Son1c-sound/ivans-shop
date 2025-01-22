8f878e591c67c3d4111e3fb3addda0a674bb5947
zura <makaradze98.s@gmail.com>
2025-01-22 06:00:28 -0500

    s

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,4 @@
-import Image from "next/image";
 import Index from "./frontend-components";
 
 
-export default function Home() {
-  return <Index />
-}
+export default function Home() { return <Index /> }
