f8f50d9815692e9b8cd99a3b441e8503302a5a71
zura <makaradze98.s@gmail.com>
2025-01-25 22:14:26 -0500

    s

--- src/app/api/stripe/route.js
+++ src/app/api/stripe/route.js
@@ -1,19 +1,19 @@
-import { NextResponse } from 'next/server';
-import Stripe from 'stripe';
+import { NextResponse } from 'next/server'
+import Stripe from 'stripe'
 
 
 const stripe = new Stripe('sk_test_51PfDj3HE0HInGjDU7SSQHixOrJRbLqkqpCfRonjhHa5V5YSQitGYHgmkVkeis3m2ioRlds8q0fzwsLRkRDVLwWAb00ECbDB5G5', {
   apiVersion: '2024-12-18.acacia',
-});
+})
 
 export async function POST(req) {
   try {
-    const { items, successUrl, cancelUrl } = await req.json();
+    const { items, successUrl, cancelUrl } = await req.json()
 
     const lineItems = items.map((item, index) => {
       if (!item.name || !item.price || !item.quantity) {
-        console.error(`Error: Missing required fields in item at index ${index}.`, item);
-        throw new Error(`Item at index ${index} is missing "name", "price", or "quantity".`);
+        console.error(`Error: Missing required fields in item at index ${index}.`, item)
+        throw new Error(`Item at index ${index} is missing "name", "price", or "quantity".`)
       }
 
       return {
@@ -27,8 +27,8 @@ export async function POST(req) {
           unit_amount: Math.round(parseFloat(item.price) * 100),
         },
         quantity: item.quantity,
-      };
-    });
+      }
+    })
 
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ['card'],
@@ -37,12 +37,12 @@ export async function POST(req) {
       success_url: successUrl,
       cancel_url: cancelUrl,
 
-    });
+    })
 
 
-    return NextResponse.json({ sessionId: session.id }, { status: 200 });
+    return NextResponse.json({ sessionId: session.id }, { status: 200 })
   } catch (error) {
-    console.error('Error creating Stripe session:', error);
+    console.error('Error creating Stripe session:', error)
     if (error instanceof Stripe.errors.StripeError) {
       return NextResponse.json(
         { error: `Stripe error: ${error.message}` },
